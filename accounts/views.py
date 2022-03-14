8af3d1d13650f21bb29f10a1437d7e3741caf3ac
ssvinay12 <101172559+ssvinay12@users.noreply.github.com>
2022-03-14 20:17:47 +0530

    mar142022
    Registration
    account activation
    SMTP
    User Login Functionality
    user activation - token based activation

--- accounts/views.py
+++ accounts/views.py
@@ -3,6 +3,16 @@ from .forms import RegistrationForm
 from .models import Account
 from django.contrib import messages,auth
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+
+
+# verification Email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
 
 
 def register(request):
@@ -18,8 +28,21 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
-            messages.success(request, 'Registration successful.')
-            return redirect('register')
+
+            # USER ACTIVATION
+            current_site = get_current_site(request)
+            mail_subject = 'please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),  #pk=primarykey
+                'token': default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
+            # messages.success(request, 'Thankyou For Registering - we have sent you a verification email to your registered email address. please verify it- Happy Shopping :-).')
+            return redirect('/accounts/login/?command=verification&email=' +email)
     else:
         form = RegistrationForm()
     context = {
@@ -36,8 +59,8 @@ def login(request):
 
         if user is not None:
             auth.login(request, user)
-            #messages.success(request, 'You are now logged in')
-            return redirect('home')
+            messages.success(request, 'You are now logged in')
+            return redirect('dashboard')
         else:
             messages.error(request, 'invalid credentials')
             return redirect('login')
@@ -49,3 +72,23 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'you are logged out.')
     return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_managet.get(pk=uid)
+    except(TypeError, ValueError, OverflowError, Account.DoesNotExists):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active = True
+        user.save()
+        message.success(request, 'congratulations! your account is activated.')
+        return redirect('login')
+    else:
+        message.error(request, 'Invalid activation link')
+        return redirect('register')
+
+@login_required(login_url = 'login')      # this dashboard will only visible if you are logged in that is why we kept this login decorator
+def dashboard(request):
+    return render (request, 'accounts/dashboard.html')
