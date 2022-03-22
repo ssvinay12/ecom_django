3a9c7d87d02b12b9757da26a918832f21d278472
ssvinay12 <101172559+ssvinay12@users.noreply.github.com>
2022-03-22 18:36:11 +0530

    allowed Host

--- ladyshoppers/settings.py
+++ ladyshoppers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG',default=True, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ladyshoppers--com-env.eba-p79hdgmy.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
